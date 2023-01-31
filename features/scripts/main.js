b163312e14b55b34c0286e9675674b5e5a83ae09
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-01-30 20:41:05 -0600

    Initial commit

--- features/scripts/main.js
+++ features/scripts/main.js
@@ -19,29 +19,23 @@ function toggleSidebar() {
 
   //ToggleContent//
   var current = "Story";
-
+  let contentTypes = ["Story", "Rides", "Features"];
   function toggleContent(request){
     if (request !== current){
-        var newContent = document.getElementById('category' + request);
-        var newButton = document.getElementById('button'+ request)
-
-        var oldContent = document.getElementById('category' + current);
-        var oldButton = document.getElementById('button' + current);
-        // brings in new content //
-        oldContent.style.opacity = "1";
-        oldContent.style.display = "none";
-        newContent.style.display = "block";
-
-        oldButton.classList = ("notSelected");
-        newButton.classList = ("isSelected");
-
-        //TODO: this could be handled better, but it's 2am and I'm tired//
-        let storyHero = document.getElementById("heroStory");
-        if(request === "Rides"){
-            storyHero.style.zIndex = "-1"
-        } else {
-            storyHero.style.zIndex = "1"
+        // hides all content //
+        for (let i = 0; i < contentTypes.length; i++) {
+            //if ("category"+request !== "category"+contentTypes[i]){
+                let pageContent = document.getElementById('category'+contentTypes[i]);
+                pageContent.style.opacity = '1';
+                pageContent.style.display = 'none';
+                document.getElementById('button'+contentTypes[i]).classList = 'notSelected';
+                document.getElementById('hero'+contentTypes[i]).style.zIndex = '-1';
+            //}
         }
+        // brings in new content //
+        document.getElementById('category' + request).style.display = "block";
+        document.getElementById('button'+ request).classList = ("isSelected");
+        document.getElementById('hero'+ request).style.zIndex = '1';
         //------------------------------------------------------------//
         current = request;
     }
