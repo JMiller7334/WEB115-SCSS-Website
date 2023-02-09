7e82330dac1e6e880d823769fa61306c4cc2cc52
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-02-09 15:59:51 -0600

    video hero bug fix fixed issue where promo video would not hide when screen size/orientation would change.
    This fix was handled using javascript. see main.js

--- features/scripts/main.js
+++ features/scripts/main.js
@@ -18,43 +18,94 @@ function toggleSidebar() {
   }
 
   //ToggleContent//
-  let current = "Story";
+  let current = "Features";
   const contentTypes = ["Story", "Rides", "Features"];
   const contentTexts = {
     Story:"Play the story, live the Adventure", 
     Rides:"Featuring over 20 epic rides", 
-    Features: "Explore a world of thrills"};
+    Features: ""};
+
+function scroll2TopContainer(container){
+    container.scrollTop = 0; // For Safari
+    container.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
   
-  function toggleContent(request){
+function toggleContent(request){
     if (request !== current){
         // hides all content //
         for (let i = 0; i < contentTypes.length; i++) {
-            //if ("category"+request !== "category"+contentTypes[i]){
-                let pageContent = document.getElementById('category'+contentTypes[i]);
-                pageContent.style.opacity = '1';
-                pageContent.style.display = 'none';
-                document.getElementById('button'+contentTypes[i]).classList = 'notSelected';
-                document.getElementById('hero'+contentTypes[i]).style.zIndex = '-1';
-            //}
+            let pageContent = document.getElementById("category"+contentTypes[i]);
+            pageContent.style.display = "none";
+            document.getElementById("button"+contentTypes[i]).classList = "notSelected";
+            document.getElementById("hero"+contentTypes[i]).style.display = "none";
         }
-        // brings in new content //
-        document.getElementById('category' + request).style.display = "block";
-        document.getElementById('button'+ request).classList = ("isSelected");
-        document.getElementById('hero'+ request).style.zIndex = '1';
-        document.getElementById('heroText').innerHTML = contentTexts[request];
+        /* brings in new content 
+        NOTE: a bit of a hack but this uses the state of the heroFeaturesMobile ID to determine if the user
+        is on a mobile device - if so we use the img hero for features.
+        */
+        if (request == "Features"){
+            //if (window.getComputedStyle(document.getElementById("heroFeaturesMobile")).display === "block"){
+            if (window.matchMedia("(max-width: 900px)").matches) {
+                console.log("jsMain: request - " + request + " using mobile hero.");
+                document.getElementById('heroFeatures').style.display = "none";
+            } else {
+                console.log("jsMain: request - " + request + " using hero.");
+                document.getElementById("heroFeatures").style.display = "block";
+            }
+        } else {
+            console.log("jsMain: request - " + request + " using non-feature hero.");
+            document.getElementById('hero'+ request).style.display = "block";
+        }
+        document.getElementById("category" + request).style.display = "block";
+        document.getElementById("button"+ request).classList = ("isSelected");
+        document.getElementById("heroText").innerHTML = contentTexts[request];
         //------------------------------------------------------------//
         current = request;
+        scroll2TopContainer(document.getElementById("contentContainer"));
     }
-  }
+}
+
+//CORRECT HERO ON WINDOW RESIZE
+const mediaQuery = "(min-width: 900px)";
+const mediaQueryList = window.matchMedia(mediaQuery);
+mediaQueryList.addEventListener('change', event => {
+    console.log(window.innerWidth);
+    if (current === "Features" ){
+        if (event.matches) {
+            //DESKTOP
+            console.log("jsMain: correcting features hero >900");
+            document.getElementById("heroFeatures").style.display = "block";
+        }else {
+            console.log("jsMain: correcting features hero <900");
+            document.getElementById("heroFeatures").style.display = "none";
+        }
+    }
+})
+
+//CORRECT HERO ON ORIENTATION CHANGE
+function onOrientationChanged(change){
+    if (current === "Features"){
+        if (change.matches) {
+            // portrait
+            console.log("jsMain: correcting features hero - display none/portrait");
+            document.getElementById("heroFeatures").style.display = "none"
+        } else {
+            // landscape
+            console.log("jsMain: correcting features hero - display none/landscape");
+            document.getElementById("heroFeatures").style.display = "block"
+        }
+    }
+};
+window.matchMedia("orientation: portrait").addEventListener("change", onOrientationChanged);
+
 
 //NOTE: func below handles clearing the intro screen after animations play.
 const contentContainer = document.getElementById("contentContainer");
-contentContainer.addEventListener('animationend', (event) => {
-    console.log("jsMain: animEnd:", event.animationName);
-    if (event.animationName === 'anim-appear'){
-        console.log('jsMain: removing introContainer')
-        if (document.getElementById('introContainer')){
-            document.getElementById('introContainer').remove();
+contentContainer.addEventListener("animationend", (event) => {
+    if (event.animationName === "anim-appear"){
+        console.log("jsMain: removing introContainer")
+        if (document.getElementById("introContainer")){
+            document.getElementById("introContainer").remove();
         }
     }
  });
